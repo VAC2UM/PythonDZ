37d0e15d6ac9eaf3a83f3ab94c4a73e16b2fa5f1
VAC2UM <nikita.golowanev@gmail.com>
2024-02-28 14:46:27 +0300

    Добавлено 3-е по поулярности решение

--- task5.py
+++ task5.py
@@ -1,13 +1,35 @@
-import math
+# Самое популярное решение
+# import math
 
 
-def main(x, y):
-    n = len(x)
-    y.insert(0, 0)
-    x.insert(0, 0)
-    summa = 0
-    for i in range(1, n + 1):
-        left = 71 * pow(y[n + 1 - math.ceil(i / 4)], 2)
-        right = 50 * pow(x[math.ceil(i / 3)], 3)
-        summa += (left - right) ** 7
-    return 35 * summa
+# def main(x, y):
+#     n = len(x)
+#     y.insert(0, 0)
+#     x.insert(0, 0)
+#     summa = 0
+#     for i in range(1, n + 1):
+#         left = 71 * pow(y[n + 1 - math.ceil(i / 4)], 2)
+#         right = 50 * pow(x[math.ceil(i / 3)], 3)
+#         summa += (left - right) ** 7
+#     return 35 * summa
+
+# print(main([0.7, -0.25, -0.01], [-0.7, -0.81, -0.63]))
+# ответ 2085404586.062392
+
+
+# Третье по популярнотси
+# import math
+
+# def main(x, y):
+#     n = len(x)
+#     y.insert(0, 0)
+#     x.insert(0, 0)
+#     summa = 0
+#     i = 1
+#     while i <= n:
+#         left = 71 * pow(y[n + 1 - math.ceil(i / 4)], 2)
+#         right = 50 * pow(x[math.ceil(i / 3)], 3)
+#         summa += (left - right) ** 7
+#         i += 1
+#     return 35 * summa
+
