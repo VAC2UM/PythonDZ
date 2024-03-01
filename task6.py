54f778e5434fd2ccb65841f84ad5a2ac05f8e92f
VAC2UM <nikita.golowanev@gmail.com>
2024-03-02 00:50:42 +0300

    Визуальные изменения

--- task6.py
+++ task6.py
@@ -3,9 +3,10 @@ import math
 
 
 def calc(A):
-    answ = 1
-    for alph in A:
-        answ *= alph % 3
+    n = len(A)
+    answ = []
+    for alph in range(1, n + 1):
+        answ.append(alph % 3)
     return answ
 
 def main(input_set):
@@ -18,13 +19,10 @@ def main(input_set):
     # вычисляем A
     A = {b ** 3 - math.floor(k / 5) for b in B for k in K if b > k}
 
-    answ = len({(a, b) for a in K for b in A}) + calc(A)
+    answ = len({(a, b) for a in K for b in A}) + math.prod(calc(A))
 
     return answ
 
-ex1 = {89, -31, -93, 69, 74, 46, 80, 87, 57, 93}
-ex2 = {-64, -54, -83, 14, 45, -16, -44, 87, -40, -36}
-
-res1 = main(ex1)
-res2 = main(ex2)
+res1 = main({89, -31, -93, 69, 74, 46, 80, 87, 57, 93})
+res2 = main({-64, -54, -83, 14, 45, -16, -44, 87, -40, -36})
 print(res1, res2)
