b801626d75da3d518c89013dfb54c427d658ff79
VAC2UM <nikita.golowanev@gmail.com>
2024-03-07 20:14:26 +0300

    Обновлен код

--- task6.py
+++ task6.py
@@ -1,4 +1,4 @@
-# пока не работает
+# Рабочее решение
 import math
 
 
@@ -10,7 +10,7 @@ def calc(A):
 
 def main(input_set):
     # вычисляем K
-    K = {v for v in input_set if 72 > v > -36}
+    K = {v for v in input_set if (v < 72 or v > -36)}
 
     # вычисляем B
     B = {abs(v) for v in input_set if -52 < v < 1e9}
@@ -18,11 +18,10 @@ def main(input_set):
     # вычисляем A
     A = {b ** 3 - math.floor(k / 5) for b in B for k in K if b > k}
 
-    # answ = len({(a, b) for a in K for b in A}) + calc(A)
-    answ = len(a * b for a in K for b in A) + calc(A)
+    answ = len({(a, b) for a in K for b in A}) + calc(A)
 
     return answ
 
-res1 = main({89, -31, -93, 69, 74, 46, 80, 87, 57, 93})
+res1 = main({89, -31, -93, 69, 74, 46, 80, 87, 57,   93})
 res2 = main({-64, -54, -83, 14, 45, -16, -44, 87, -40, -36})
 print(res1, res2)
