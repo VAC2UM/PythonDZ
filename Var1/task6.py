48c2310e3f2dde0f46222d588d332be7930db05a
VAC2UM <nikita.golowanev@gmail.com>
2024-03-29 23:45:36 +0300

    Добавлены тестовые решения

--- Var1/task6.py
+++ Var1/task6.py
@@ -1,9 +1,7 @@
-import math
-
 def main(P):
-    A = {p % 3 - 4 * p for p in P if not (-1e9 < p < -25)}
+    A = {(p % 3 - 4 * p) for p in P if not (-1e9 < p < -25)}
 
-    I = {p ** 3 for p in P if p >= 31 and p <= 60}
+    I = {pow(p, 3) for p in P if (p >= 31 and p <= 60)}
 
     M = I.union(A)
 
@@ -12,6 +10,7 @@ def main(P):
     answ = len(M) - sum(y - 3 * x for y in M for x in X)
 
     return answ
+
 
 res1 = main({70, 42, 45, -13, 52, 85, -41, -39, 90, -68})
 res2 = main({-61, 70, -20, 92, -48, -49, 86, -40, 60, -97})
