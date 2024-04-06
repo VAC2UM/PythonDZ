b19e116050b446fe777976f399d6b4d8344ab101
VAC2UM <nikita.golowanev@gmail.com>
2024-04-06 14:02:25 +0300

    Добавлено 1-е решение

--- task9.py
+++ task9.py
@@ -1,16 +1,35 @@
-def main(x):
-    x = x.replace('(', '').replace(')', '').replace('var', '').replace('\n', '').replace(' ', '').replace('|', '').replace('.option', '').replace('.begin', '').replace('.end', '')
-    x_parts = x.split(':')
-    x_parts = [i for i in x_parts if i]  # удаляем пустые строки из списка
-    x_parts1 = []
-    for i in x_parts:
-        if ':' in i:
-            x_parts1.append(i.split(':'))
-        else:
-            print(f"Ошибка: в строке '{i}' отсутствует двоеточие")
+# Самое популярное  решение
+# -------------------------------------------------------------------
+# import re
+#
+#
+# def main(data):
+#     # Регулярное выражение для извлечения ключей и значений
+#     pattern = r'option\s+([\w_]+)\s*:\s*([\w_]+)'
+#
+#     # Используем findall для поиска всех совпадений
+#     matches = re.findall(pattern, data)
+#
+#     # Создаем словарь из найденных совпадений
+#     result = {key: value for key, value in matches}
+#
+#     return result
+# -------------------------------------------------------------------
+
+
+def main(input_string):
     result = {}
-    for i in x_parts1:
-        result[i[0]] = i[1]
+    input_string = input_string.replace('.begin option', ' ')
+    input_string = input_string.replace('. option', ',')
+    input_string = input_string.replace(':', ':')
+    input_string = input_string.replace('. .end', '')
+
+    pairs = input_string.split(',')
+    for pair in pairs:
+        key, value = pair.split(':')
+        result[key.strip()] = value.strip()
+
     return result
 
-print(main(".begin option abe_117 : ceis. option radior_155 :laenxe_853. option reza_100 :tiat_46. option inzaso_768: sosous_708. .end"))
+
+print(main('.begin option abe_117 : ceis. option radior_155 :laenxe_853. option reza_100 :tiat_46. option inzaso_768: sosous_708. .end'))
